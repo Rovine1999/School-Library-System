423ab8bdd92661d0c50f7eceae842da322a9091a
Rovine1999 <rovinewanjala99@gmail.com>
2021-02-02 00:35:06 +0300

    import create user form from forms

--- mylibrary/views.py
+++ mylibrary/views.py
@@ -4,7 +4,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from .models import Books
-from django.views.generic import CreateView, ListView,DetailView,UpdateView,DeleteView
+from .forms import CreateUserForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView,ListView,DetailView,UpdateView,DeleteView
 
 # Create your views here.
 @login_required(login_url='login')
@@ -23,7 +25,7 @@ def registerPage(request):
             form =CreateUserForm(request.POST)
             if form.is_valid():
                 form.save()
-                messages.success(request,'Account was created successfully')
+                messages.success(request,'Account was successfully created')
                 return redirect('login')
         context = {'form': form}
     return render(request,'registration/register.html',  context)
@@ -44,7 +46,7 @@ def loginPage(request):
 def logoutUser(request):
     logout(request)
     return redirect('login')
-class LibraryCreateView(LoginRequiredMixin,CreateView):
+class BooksCreateView(LoginRequiredMixin,CreateView):
     model = Books
     fields = ['images', 'title', 'description', 'category']
     template_name = 'add.html'
@@ -52,4 +54,40 @@ class LibraryCreateView(LoginRequiredMixin,CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class BooksCreateView(LoginRequiredMixin,CreateView):
+    model = Books
+    fields = ['images', 'title', 'description', 'category']
+    template_name = 'post.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+# <app>/<model>_<viewtype>.html
+class BooksListView(ListView):
+    model = Books  
+    template_name = 'index.html'   
+    context_object_name = 'flashcards'
+    ordering = ['-pub_date']
+
+class BooksCreateView(CreateView):
+    model = Books
+    template_name = 'post.html'   
+    fields= ['title', 'description','category']    
+
+class BooksUpdateView(UpdateView):
+    model = Books
+    template_name = 'post.html'   
+    fields= ['title', 'description','category']    
+
+class BooksDeleteView(DeleteView):
+    model = Books
+    template_name = 'delete.html'
+    success_url = ('/')
+
+def deleteForm(request):
+    context ={     
+    }
+    return render(request ,'delete.html', context )    
 
