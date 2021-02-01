55358fce0642753d5c22dc21fa18333a177993f5
Rovine1999 <rovinewanjala99@gmail.com>
2021-02-02 00:27:51 +0300

    add url for the index for display page

--- mylibrary/urls.py
+++ mylibrary/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, re_path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import LibraryCreateView,LibraryListView,LibraryUpdateView,LibraryDeleteView
+from .views import BooksCreateView,BooksListView,BooksUpdateView,BooksDeleteView
 
 
 urlpatterns=[
-    path('',LibraryListView.as_view(),name = 'index'),
+    path('',views.index,name = 'index'),
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
