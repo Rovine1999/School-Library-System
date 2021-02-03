68b60dd655c102f3ef74de9f9023a0a17abc16cf
Rovine1999 <rovinewanjala99@gmail.com>
2021-02-04 02:30:46 +0300

    added field author to the model for user

--- mylibrary/models.py
+++ mylibrary/models.py
@@ -11,6 +11,7 @@ from django.db.models.signals import post_save
 class Books(models.Model):
     title = models.CharField(max_length=50, blank=True, null=True)
     name = models.CharField(max_length =30)
+    writer = models.CharField(max_length =50)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="books", blank=True,null=True)
     description = models.TextField(max_length =200, blank=True, null=True)
     pub_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
@@ -33,3 +34,8 @@ class Books(models.Model):
         
     def get_absolute_url(self):
         return reverse('index')
+
+    @classmethod
+    def search_category(cls,search_term):
+        searched_category = Books.objects.filter(name =search_term)
+        return searched_category
