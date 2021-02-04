fffd700aa2e72b80bfa4a62c07ce7febc883e5df
Rovine1999 <rovinewanjala99@gmail.com>
2021-02-04 14:19:24 +0300

    added user update form model for user to update profile

--- mylibrary/forms.py
+++ mylibrary/forms.py
@@ -2,10 +2,23 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
 from django.contrib.auth.models import User
-from .models import Books
+from .models import Books,Profile
 
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'location', 'profile_picture', 'bio', 'email']
